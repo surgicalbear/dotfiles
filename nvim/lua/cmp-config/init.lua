3d53d3da585c99182cdb387fe49f1c471bf41395
bear <isadelski@gmail.com>
2024-03-21 16:23:44 -0400

    added starship and changed theme

--- nvim/lua/cmp-config/init.lua
+++ nvim/lua/cmp-config/init.lua
@@ -75,6 +75,9 @@ local cmp = require'cmp'
     require'lspconfig'.pylsp.setup {
         capabilities = capabilities
     }
+    require'lspconfig'.jdtls.setup {
+        capabilities = capabilities 
+    }
 
 local lspconfig = require('lspconfig')
 local configs = require('lspconfig/configs')
